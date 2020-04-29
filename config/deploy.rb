cf6d1b9538d6ecbce87590031d652340132c60a3
hisnuinn333 <tatsuro11111133@gmail.com>
2020-04-29 10:36:30 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,6 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.13.0'
 
-# secrets.yml用のシンボリックリンクを追加
-set :linked_files, %w{ config/secrets.yml }
-
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
 
@@ -27,6 +24,9 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 # Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
